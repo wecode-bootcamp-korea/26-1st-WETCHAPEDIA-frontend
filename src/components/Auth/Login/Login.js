12033dca425f1c39d504fea766515a38d6c1e68f
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2021-11-02 21:45:50 +0900

    add : signup/login 레이아웃 구현

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react';
+import './sample.scss';
 
-class Login extends Component {
+class sample extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleEmailInput = e => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  handlePwdInput = e => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
   render() {
-    return <div>수정용입니다</div>;
+    return (
+      <div className="loginContainer">
+        <div className="wechaContainer">
+          <span className="initial">wechachapedia</span>
+          <span className="loginMark">로그인</span>
+        </div>
+        <form className="signupContainer">
+          <input
+            className="emailLoginBox"
+            placeholder="이메일"
+            onChange={this.handleEmailInput}
+          />
+          <input
+            className="passwordLoginBox"
+            placeholder="비밀번호"
+            onChange={this.handlePwdInput}
+          />
+          <button className="buttonLoginBox">로그인</button>
+        </form>
+        <div className="forgetLoginContainer">
+          <span className="forgetPwd">비밀번호를 잊어버리셨나요?</span>
+          <span className="signupLoginText">계정이 없으신가요? 회원가입</span>
+        </div>
+        <hr className="divideSocialLine" />
+
+        <button
+          className="socialLoginButton"
+          disabled={
+            !(
+              this.state.email.indexOf('@') !== -1 &&
+              this.state.password.length > 5
+            )
+          }
+        >
+          <img />
+          Facebook 으로 로그인
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+export default sample;
--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,8 +1,67 @@
 import React, { Component } from 'react';
+import './Register.scss';
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+    };
+  }
+  handleNameInput = e => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+  handleEmailInput = e => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  handlePwdInput = e => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="SignupContainer">
+        <div className="logoSection">
+          <span className="initialName">wechachapedia</span>
+          <span className="Logologin">로그인</span>
+        </div>
+        <form className="enterContainer">
+          <input
+            className="nameBox"
+            placeholder="이름"
+            onChange={this.handleNameInput}
+          />
+          <input
+            className="emailBox"
+            placeholder="이메일"
+            onChange={this.handleEmailInput}
+          />
+          <input
+            className="passwordBox"
+            placeholder="비밀번호"
+            onChange={this.handlePwdInput}
+          />
+          <button className="buttonBox">로그인</button>
+        </form>
+        <div className="forgetContainer">
+          <span>이미 가입하셨나요?</span>
+          <span className="signupText">로그인</span>
+        </div>
+        <hr className="divideLine" />
+
+        <button className="socialLogin">
+          <img />
+          Facebook 으로 로그인
+        </button>
+      </div>
+    );
   }
 }
 
