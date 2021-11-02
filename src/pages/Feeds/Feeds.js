b359782cdebc60d563db1fb8c762de426d037630
choi2601 <65222200+choi2601@users.noreply.github.com>
2021-11-03 02:47:42 +0900

    [26기 최병현]Modify: 마크업 및 스타일시트 재구성 / slide 기능 구현

--- src/pages/Feeds/Feeds.js
+++ src/pages/Feeds/Feeds.js
@@ -1,57 +1,216 @@
 import React, { Component } from 'react';
 import './Feeds.scss';
 
+function getTarget(elem, className) {
+  while (!elem.classList.contains(className)) {
+    elem = elem.parentNode;
+
+    if (elem.nodeName === 'BODY') {
+      elem = null;
+      return;
+    }
+  }
+  return elem;
+}
+
 class Feeds extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      moveX: 0,
+    };
+  }
+
+  moveToX = event => {
+    let { moveX } = this.state;
+    let { name } = getTarget(event.target, 'carouselControl');
+
+    name === 'right'
+      ? this.setState({ moveX: moveX - 50 })
+      : this.setState({ moveX: moveX + 50 });
+  };
+
   render() {
+    let { moveX } = this.state;
+
     return (
-      <div className="feeds">
-        <div className="feedList">
-          <div className="categoryContainer">
-            <p className="categoryName">박스오피스 순위</p>
+      <article className="feeds">
+        <section className="feedsContainer">
+          <div className="feedsCategoryContainer">
+            <p className="feedsCategory">박스오피스 순위</p>
           </div>
-          <div className="feedShow">
-            <div className="feedSlide">
-              <div className="feedContainer">
-                <div className="feed">
-                  <figure className="moviePoster" />
-                  <div className="movieInfo">
-                    <p className="movieTitle">이터널스</p>
-                    <p className="movieTear">2021 ・ 미국</p>
-                  </div>
+          <div className="feedsCarousel">
+            <ul
+              className="carouselSlider"
+              style={{ transform: `translate3D(${moveX}%, 0, 0)` }}
+            >
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
                 </div>
-                <div className="feed">
-                  <figure className="moviePoster" />
-                  <div className="movieInfo">
-                    <p className="movieTitle">이터널스</p>
-                    <p className="movieTear">2021 ・ 미국</p>
-                  </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
                 </div>
-                <div className="feed">
-                  <figure className="moviePoster" />
-                  <div className="movieInfo">
-                    <p className="movieTitle">이터널스</p>
-                    <p className="movieTear">2021 ・ 미국</p>
-                  </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
                 </div>
-                <div className="feed">
-                  <figure className="moviePoster" />
-                  <div className="movieInfo">
-                    <p className="movieTitle">이터널스</p>
-                    <p className="movieTear">2021 ・ 미국</p>
-                  </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
                 </div>
-                <div className="feed">
-                  <figure className="moviePoster" />
-                  <div className="movieInfo">
-                    <p className="movieTitle">이터널스</p>
-                    <p className="movieTear">2021 ・ 미국</p>
-                  </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
                 </div>
-              </div>
-            </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">베놈</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">베놈</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">베놈</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">베놈</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">베놈</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+            </ul>
           </div>
-        </div>
-      </div>
+          <button
+            onClick={this.moveToX}
+            name="right"
+            className="carouselControl nextBtn"
+          >
+            <div className="btnImg" />
+          </button>
+        </section>
+        <section className="feedsContainer">
+          <div className="feedsCategoryContainer">
+            <p className="feedsCategory">왓차 영화 순위</p>
+          </div>
+          <div className="feedsCarousel">
+            <ul className="carouselSlider">
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">이터널스</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+              <li className="feed">
+                <figure className="moviePoster" />
+                <div className="movieInfo">
+                  <p className="movieTitle">베놈</p>
+                  <p className="movieTear">2021 ・ 미국</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <button
+            onClick={this.moveToX}
+            name="right"
+            className="carouselControl nextBtn"
+          >
+            <div className="btnImg" />
+          </button>
+        </section>
+      </article>
     );
   }
 }
