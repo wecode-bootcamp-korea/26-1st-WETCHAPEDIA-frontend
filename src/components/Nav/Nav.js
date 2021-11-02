002e123f82712cb3c4adff737172a9635c56f0ee
정지후 <loveadsadf1976@gmail.com>
2021-11-02 10:52:23 +0900

    Add: feature/NavFooter branch생성

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import '../../styles/common.scss';
+import '../Nav/Nav.scss';
 
 class Nav extends Component {
   render() {
-    return <div>jihoo</div>;
+    return (
+      <header>
+        <nav>
+          <div className="sdkj">{/* <p>dkja;f</p> */}</div>
+        </nav>
+      </header>
+    );
   }
 }
 
