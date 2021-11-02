6c191330e92a2dc56a4b8441ff503afef723a4b1
choi2601 <65222200+choi2601@users.noreply.github.com>
2021-11-02 20:42:17 +0900

    [26기 최병현]Add: feeds 초기 layout 추가

--- src/pages/Feeds/Feeds.js
+++ src/pages/Feeds/Feeds.js
@@ -3,7 +3,56 @@ import './Feeds.scss';
 
 class Feeds extends Component {
   render() {
-    return <div className="test">Feeds page</div>;
+    return (
+      <div className="feeds">
+        <div className="feedList">
+          <div className="categoryContainer">
+            <p className="categoryName">박스오피스 순위</p>
+          </div>
+          <div className="feedShow">
+            <div className="feedSlide">
+              <div className="feedContainer">
+                <div className="feed">
+                  <figure className="moviePoster" />
+                  <div className="movieInfo">
+                    <p className="movieTitle">이터널스</p>
+                    <p className="movieTear">2021 ・ 미국</p>
+                  </div>
+                </div>
+                <div className="feed">
+                  <figure className="moviePoster" />
+                  <div className="movieInfo">
+                    <p className="movieTitle">이터널스</p>
+                    <p className="movieTear">2021 ・ 미국</p>
+                  </div>
+                </div>
+                <div className="feed">
+                  <figure className="moviePoster" />
+                  <div className="movieInfo">
+                    <p className="movieTitle">이터널스</p>
+                    <p className="movieTear">2021 ・ 미국</p>
+                  </div>
+                </div>
+                <div className="feed">
+                  <figure className="moviePoster" />
+                  <div className="movieInfo">
+                    <p className="movieTitle">이터널스</p>
+                    <p className="movieTear">2021 ・ 미국</p>
+                  </div>
+                </div>
+                <div className="feed">
+                  <figure className="moviePoster" />
+                  <div className="movieInfo">
+                    <p className="movieTitle">이터널스</p>
+                    <p className="movieTear">2021 ・ 미국</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
