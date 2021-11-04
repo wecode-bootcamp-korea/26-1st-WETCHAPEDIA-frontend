12f9bb0b5096be8cca2c75016f911ec0c79035ec
정지후 <loveadsadf1976@gmail.com>
2021-11-04 21:46:19 +0900

    Modify: Nav와 Footer 리팩토링 수정 완료

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,9 @@ import { EMAILINFO, INFO } from './footerData';
 import '../Footer/Footer.scss';
 
 class Footer extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
   render() {
     return (
-      <footer className="Footer">
+      <footer className="footer">
         <div className="topBanner">
           <span>
             지금까지 <em>★ 635,684,311 개의 평가가</em> 쌓였어요.
@@ -17,12 +13,12 @@ class Footer extends Component {
         </div>
         <div className="footerMain">
           <section className="leftFooter">
-            <ul className="ftFirstList">
+            <ul className="firstList">
               <li>서비스 이용약관</li>
               <li>개인정보 처리방침</li>
               <li>회사 안내</li>
             </ul>
-            <ul className="ftSecondList">
+            <ul className="secondList">
               {EMAILINFO.map(email => {
                 return (
                   <li className="emailList" key={email.id}>
@@ -31,7 +27,7 @@ class Footer extends Component {
                 );
               })}
             </ul>
-            <ul className="ftThirdList">
+            <ul className="thirdList">
               {INFO.map(member => {
                 return (
                   <li className="memberList" key={member.id}>
@@ -47,7 +43,7 @@ class Footer extends Component {
             </ul>
           </section>
           <section className="rightFooter">
-            <div className="ftList">
+            <div className="list">
               <div className="select">
                 <span>한국어</span>
               </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,44 +1,91 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { SEARCHLIST } from './navData.js';
 import '../Nav/Nav.scss';
 
 class Nav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      seen: false,
+    };
+  }
+
+  handleInput = () => {
+    this.setState({
+      seen: !this.state.seen,
+    });
+  };
+
+  open = () => {
+    this.setState({
+      seen: true,
+    });
+  };
+
+  close = () => {
+    this.setState({
+      seen: false,
+    });
+  };
+
   render() {
     return (
-      <header>
-        <nav>
-          <div className="navLeft">
-            <div className="navBar">
-              <h3 className="fontLogo">
-                Wetcha <span className="smallLogo">pedia</span>
-              </h3>
-              <div className="navigation">
-                <p>영화</p>
-                <p>TV 프로그램</p>
-                <p>책</p>
+      <>
+        <header>
+          <nav>
+            <div className="navLeft">
+              <div className="navBar">
+                <h3 className="fontLogo">
+                  Wetcha <span className="smallLogo">pedia</span>
+                </h3>
+                <div className="navigation">
+                  <p>영화</p>
+                  <p>TV 프로그램</p>
+                  <p>책</p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="navRight">
-            <div className="searchWrapper">
-              <form className="searchbar">
-                <input
-                  type="search"
-                  id="id"
-                  placeholder="콘텐츠,인물,컬렉션,유저를 검색해보세요."
-                />
-              </form>
+            <div className="navRight">
+              <div className="searchWrapper">
+                <form className="searchbar">
+                  <input
+                    type="search"
+                    id="id"
+                    placeholder="콘텐츠,인물,컬렉션,유저를 검색해보세요."
+                    autoComplete="off"
+                    onClick={this.handleInput}
+                  />
+                </form>
+
+                {this.state.seen ? (
+                  <div className="searchbarList">
+                    <div className="searchListDown">
+                      <p>인기 검색어</p>
+                      {SEARCHLIST.map(list => {
+                        return (
+                          <h3 className="searchList" key={list.id}>
+                            {list.content}
+                          </h3>
+                        );
+                      })}
+                    </div>
+                  </div>
+                ) : null}
+              </div>
+              <div className="loginBtn">
+                <button>로그인</button>
+              </div>
+              <div className="signBtn">
+                <button>회원가입</button>
+              </div>
             </div>
-            <div className="loginBtn">
-              <button>로그인</button>
-            </div>
-            <div className="signBtn">
-              <button>회원가입</button>
-            </div>
-          </div>
-        </nav>
-      </header>
+          </nav>
+        </header>
+        {this.state.seen ? (
+          <div onClick={this.handleInput} className="headerOverlay" />
+        ) : null}
+      </>
     );
   }
 }
